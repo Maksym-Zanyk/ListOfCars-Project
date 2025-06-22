85af102b6a72be0e7b4f8bbac3835a8d05bc9dfa
Maksym-Zanyk <258469@edu.p.lodz.pl>
2025-06-22 21:27:04 +0300

    Added all functionalities of the list

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 void menu(){
     cout << "1. All cars\n";
     cout << "2. Add a car\n";
-    cout << "3. Find a car\n";
+    cout << "3. Find cars\n";
     cout << "4. Edit a car by index\n";
     cout << "5. Save data to file\n";
     cout << "6. Load data from file\n";
+    cout << "7. Delete a car from list\n";
     cout << "0. Exit\n\n";
 
 
@@ -39,23 +40,31 @@ int main()
 
 
             case 2:
-                //ReallocateTheList(list_of_cars, current_number_of_cars);
-                //EnterCarToList(list_of_cars, current_number_of_cars);
+                EnterCarToList();
                 break;
 
             case 3:
+                FindCars();
                 break;
 
             case 4:
+                if(EditCarByIndex() == 0) cout << "Edited successfuly!\n" << endl;
                 break;
 
             case 5:
+                if(SaveListToFile() == 0) cout << "Saved successfuly!\n" << endl;
                 break;
 
             case 6:
+                if(LoadListFromFile() == 0) cout << "Loaded successfuly!\n" << endl;
+                break;
+                
+            case 7:
+                if(DeleteCarFromList() == 0){ cout << "Deleted successfuly!\n" << endl; }
                 break;
 
-            case 7:
+            default:
+                cout << "No such option" << endl;
                 break;
         }
 
@@ -64,10 +73,3 @@ int main()
 
     return 0;
 }
-
-
-
-
-
-
-
